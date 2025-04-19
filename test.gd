705f01755d5bd4b9f91bfb9fa9c1eb6fc3fb96cc
Christian Halim <chris.richard.halim@gmail.com>
2025-04-19 22:05:24 +0700

    Prototype board

--- test.gd
+++ test.gd
@@ -1,17 +1,2 @@
 extends Node
-
-@onready var cells = $BoardContainer.get_children()
-var reveal_delay = 1 # seconds between reveals
-
-func _ready():
-	# Hide all cells at start
-	for cell in cells:
-		cell.visible = false
-	# Start the reveal animation
-	reveal_cells()
-
-func reveal_cells():
-	# Coroutine to reveal each cell one by one
-	for cell in cells:
-		cell.visible = true
-		await get_tree().create_timer(reveal_delay).timeout
+@onready var game_manager: Node = $"../GameManager"
